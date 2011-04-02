5d615cf47b6cb99437c2f5054ae3ab40f5c0f036
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2011-04-02 23:04:37 +0200

    * Big refactoring to have one routine to retrieve and store DNS data. I hope it broke nothing... * Allows us to query NS as well * Better documentation

--- key-store-and-report.py
+++ key-store-and-report.py
@@ -26,7 +26,7 @@ maintainer_address = "foo@bar"
 output = True
 syslog = False
 SECTION = "default"
-version = sys.argv[0] + " $Revision: 10531 $ (Python %s)" % \
+version = sys.argv[0] + " $Revision: 10620 $ (Python %s)" % \
           re.sub ("\n", " ", sys.version)
 # A sample configuration file:
 # [default]
@@ -39,6 +39,11 @@ version = sys.argv[0] + " $Revision: 10531 $ (Python %s)" % \
 class DNSerror(Exception):
     pass
 
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+nullh = NullHandler()
+
 def sendemail(subject, content):
     msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\nX-Mailer: %s\r\nMIME-Version: 1.0\r\nContent-type: text/plain; charset=\"UTF-8\"\r\n\r\n" % \
            (maintainer_address, maintainer_address, ("[%s] " % email_prefix) + subject,
@@ -48,6 +53,84 @@ def sendemail(subject, content):
     server.set_debuglevel(0)
     server.sendmail(maintainer_address, maintainer_address, msg)
     server.quit()
+
+def get_rr(zone, rrtype, ns_address, handler=None):
+    """ rrtype must be a character _string_. handler is a function
+    which will be called for each rrset (and receives it as a
+    parameter). See display_target for an example of handker (it is
+    suitable for a NS rrtype)."""
+    mytype = dns.rdatatype.from_text(rrtype)
+    query = dns.message.make_query(zone, rrtype)
+    query.use_edns(edns=True, payload=edns_size)
+    query.want_dnssec(True)
+    tests = 0
+    while tests < max_tests:
+        try:
+            response = dns.query.udp(query, ns_address, timeout=2)
+            break
+        except dns.exception.Timeout:
+            tests += 1
+            time.sleep(mytimeout * generator.randint(1, 3))
+    if tests >= max_tests:
+        # TODO: try with TCP, even without truncation? It seems to
+        # work with Afilias servers
+        default_log.warning("Timeout on %s query for %s on %s" % (rrtype, zone, address))
+        sys.exit(1)
+    if response.flags & dns.flags.TC: # We were truncated
+        response = dns.query.tcp(query, address, timeout=2*mytimeout)
+        # TODO: handle the case where the nameserver is broken enough to
+        # truncate responses *and* to refuse TCP
+    record_found = False
+    cursor.execute("BEGIN IMMEDIATE TRANSACTION;"); 
+    for rrset in response.answer:
+        if rrset.rdtype == mytype:
+            record_found = True
+            if handler:
+                handler(rrset)
+        elif rrset.rdtype == dns.rdatatype.RRSIG:
+            for thesig in rrset:
+                if mytype != dns.rdatatype.DNSKEY and thesig.key_tag not in key_tags:
+                     default_log.error("Signed with %s which is not in the key set" % \
+                                   thesig.key_tag)
+                sig_value = base64.b64encode(thesig.signature)
+                cursor.execute("SELECT last_seen FROM Signatures WHERE signature=?;", (sig_value,))
+                tuple = cursor.fetchone()
+                if tuple is None:   
+                    # TODO: store inception and expiration as actual times, not integers
+                    cursor.execute("INSERT INTO Signatures (first_seen, last_seen, type, name, ttl, key_tag, algorithm, inception, expiration, signature) VALUES (datetime('now'), datetime('now'), ?, ?, ?, ?, ?, ?, ?, ?);", \
+                                   (mytype, zone, rrset.ttl, thesig.key_tag,
+                                    thesig.algorithm, thesig.inception, thesig.expiration, sig_value))
+                else:
+                    cursor.execute("UPDATE Signatures SET last_seen=datetime('now') WHERE signature=?;",
+                                   (sig_value,))
+    if not record_found:
+        default_log.error("No %s at %s" % (rrtype, zone))
+        sys.exit(1)
+    database.commit()
+
+def display_target(set):
+    """ Example of a simple handker for get_rr. """
+    for record in set:
+        print record.target
+
+def create_key_list(set):
+    global keys
+    global dnskey_ttl
+    keys = []
+    dnskey_ttl = set.ttl
+    for thekey in set:
+        keys.append(thekey)
+
+def update_zones(set):
+    serial = set[0].serial
+    cursor.execute("SELECT serial FROM Zones WHERE name=? ORDER BY last_seen DESC LIMIT 1;", (zone,))
+    tuple = cursor.fetchone()
+    if tuple is None or serial != tuple[0]: 
+        cursor.execute("INSERT INTO Zones (first_seen, last_seen, name, serial, nameserver) VALUES (datetime('now'), datetime('now'), ?, ?, ?);" , \
+                       (zone, serial, address))
+    else:
+        cursor.execute("UPDATE Zones SET last_seen=datetime('now'), nameserver=? WHERE name=? AND serial=?;", \
+                       (address, zone, serial))
         
 if len(sys.argv) != 3:
     raise Exception("Usage: dnssec.py zonename nameserver-address")
@@ -83,6 +166,8 @@ if syslog:
     ch = logging.handlers.SysLogHandler("/dev/log")
     ch.setFormatter(formatter_short)
     default_log.addHandler(ch)
+if not output and not syslog:
+    default_log.addHandler(nullh)
 zone = sys.argv[1]
 if zone[-1] != '.':
     zone += '.'
@@ -91,63 +176,19 @@ address = sys.argv[2]
 # the real clock, to test "what if" scenarios?
 default_log.info("Starting %s (%s)..." % (zone, address))
 error = None
-keys = []
-key_tags = []
 database = sqlite3.connect(database_name) # No need to lock
                                           # ourselves. http://www.sqlite.org/faq.html#q5
 cursor = database.cursor()
 addresses = []
 # TODO: read the database to retrieve the former serial number, to see
 # if there was any change?
-query = dns.message.make_query(zone, 'DNSKEY')
-query.use_edns(edns=True, payload=edns_size)
-query.want_dnssec(True)
-query2 = dns.message.make_query(zone, 'SOA')
-query2.use_edns(edns=True, payload=edns_size)
-query2.want_dnssec(True)
-tests = 0
-while tests < max_tests:
-    try:
-        response = dns.query.udp(query, address, timeout=mytimeout)
-        break
-    except dns.exception.Timeout:
-        tests += 1
-        time.sleep(mytimeout * generator.randint(1, 3))
-if tests >= max_tests:
-    default_log.warning("Timeout on DNSKEY UDP query for %s on %s" % (zone, address))
-    # TODO: try with TCP? It seems to work with Afilias servers
-    sys.exit(1)
-dnskey_found = False
-if response.flags & dns.flags.TC: # We were truncated
-    response = dns.query.tcp(query, address, timeout=2*mytimeout)
-    # TODO: handle the case where the nameserver is broken enough to
-    # truncate responses *and* to refuse TCP
-cursor.execute("BEGIN IMMEDIATE TRANSACTION;"); 
-for rrset in response.answer:
-    if rrset.rdtype == dns.rdatatype.DNSKEY:
-        dnskey_found = True
-        dnskey_ttl = rrset.ttl
-        for thekey in rrset:
-            keys.append(thekey)
-    elif rrset.rdtype == dns.rdatatype.RRSIG:
-        for thesig in rrset:
-            # TODO: wait until we are sure we've found a DNSKEY
-            # TODO: use our own clock rather than SQLite 'now' since
-            # we may have to store the time several times
-            sig_value = base64.b64encode(thesig.signature)
-            cursor.execute("SELECT last_seen FROM Signatures WHERE signature=?;", (sig_value,))
-            tuple = cursor.fetchone()
-            if tuple is None: 
-                # TODO: store inception and expiration as actual times, not integers
-                cursor.execute("INSERT INTO Signatures (first_seen, last_seen, type, name, ttl, key_tag, algorithm, inception, expiration, signature) VALUES (datetime('now'), datetime('now'), ?, ?, ?, ?, ?, ?, ?, ?);", \
-                               (dns.rdatatype.DNSKEY, zone, rrset.ttl, thesig.key_tag,
-                                thesig.algorithm, thesig.inception, thesig.expiration, sig_value))
-            else:
-                cursor.execute("UPDATE Signatures SET last_seen=datetime('now') WHERE signature=?;", (sig_value,))
-if not dnskey_found:
-    default_log.error("No DNSKEY at %s (for %s)" % (address, zone))
-    sys.exit(1)
+
+# TODO: use our own clock rather than SQLite 'now' since
+            
+get_rr(zone, 'DNSKEY', address, create_key_list)
+
 keys.sort()
+key_tags = []
 hasher = hashlib.sha1()
 for key in keys:
     key_tag = dns.dnssec.key_id(key)
@@ -183,51 +224,11 @@ if tuple is None:
     sendemail("New keyset in zone %s" % zone, infos)
 else: # This is an already-seen keyset
     cursor.execute("UPDATE Keysets SET last_seen=datetime('now'), ttl=? WHERE id=? AND name=?;", (dnskey_ttl, dnskey_id, zone))
-tests = 0
-database.commit()
-while tests < max_tests:
-    try:
-        response = dns.query.udp(query2, address, timeout=2)
-        break
-    except dns.exception.Timeout:
-        tests += 1
-        time.sleep(mytimeout * generator.randint(1, 3))
-if tests >= max_tests:
-    default_log.warning("Timeout on SOA query for %s on %s" % (zone, address))
-    sys.exit(1)
-soa_found = False
-cursor.execute("BEGIN IMMEDIATE TRANSACTION;"); 
-for rrset in response.answer:
-    if rrset.rdtype == dns.rdatatype.SOA:
-        soa_found = True
-        serial = rrset[0].serial
-        cursor.execute("SELECT serial FROM Zones WHERE name=? ORDER BY last_seen DESC LIMIT 1;", (zone,))
-        tuple = cursor.fetchone()
-        if tuple is None or serial != tuple[0]: 
-            cursor.execute("INSERT INTO Zones (first_seen, last_seen, name, serial, nameserver) VALUES (datetime('now'), datetime('now'), ?, ?, ?);" , \
-                           (zone, serial, address))
-        else:
-            cursor.execute("UPDATE Zones SET last_seen=datetime('now'), nameserver=? WHERE name=? AND serial=?;", \
-                           (address, zone, serial))
-    elif rrset.rdtype == dns.rdatatype.RRSIG:
-        for thesig in rrset:
-            if thesig.key_tag not in key_tags:
-                 default_log.error("Signed with %s which is not in the key set" % \
-                               thesig.key_tag)
-            sig_value = base64.b64encode(thesig.signature)
-            cursor.execute("SELECT last_seen FROM Signatures WHERE signature=?;", (sig_value,))
-            tuple = cursor.fetchone()
-            if tuple is None:   
-                cursor.execute("INSERT INTO Signatures (first_seen, last_seen, type, name, ttl, key_tag, algorithm, inception, expiration, signature) VALUES (datetime('now'), datetime('now'), ?, ?, ?, ?, ?, ?, ?, ?);", \
-                               (dns.rdatatype.SOA, zone, rrset.ttl, thesig.key_tag,
-                                thesig.algorithm, thesig.inception, thesig.expiration, sig_value))
-            else:
-                cursor.execute("UPDATE Signatures SET last_seen=datetime('now') WHERE signature=?;",
-                               (sig_value,))
-if not soa_found:
-    default_log.error("No SOA at %s" % rdata.target)
-    sys.exit(1)
-database.commit()
+
+get_rr(zone, 'SOA',  address, update_zones)
+
+get_rr(zone, 'NS',  address)
+
 cursor.close()
 database.close()
 default_log.info("Successfully done for %s (%s)" % (zone, address))
