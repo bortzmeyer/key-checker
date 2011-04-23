24aabcdff2f4cd78438cda446e664cfa4b370338
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2011-04-23 15:28:16 +0200

    Double-signature (something which is uncommon but can be found in isc.org, or infoblox.com) now ignored

--- examine-history.py
+++ examine-history.py
@@ -5,6 +5,7 @@ followed the proper timing."""
 
 database_name = "dnssec.sqlite"
 verbose = False
+order = 'time' # 'time' or 'name'
 
 import sys
 import sqlite3
@@ -14,6 +15,8 @@ zone_cursor = database.cursor()
 key_cursor = database.cursor()
 keyset_cursor = database.cursor()
 sig_cursor = database.cursor()
+resig_cursor = database.cursor()
+problems = {}
 
 def test_zone(zone):
     if verbose:
@@ -53,7 +56,7 @@ def test_zone(zone):
                 if verbose:
                     print "\t\tLast previous keyset before %s: %s (TTL %i)" % (keyset, last, ttl)
             if reference_time is not None:
-                sig_cursor.execute("""SELECT first_seen FROM Signatures
+                sig_cursor.execute("""SELECT first_seen, type FROM Signatures
                                           WHERE key_tag=? AND name=?
                                              AND type != 48 
                                              AND datetime(strftime('%s',first_seen),
@@ -64,6 +67,7 @@ def test_zone(zone):
                                    [key_tag, zone, reference_time, ttl])
                 for sig_tuple in sig_cursor.fetchall():
                     first_sig = sig_tuple[0]
+                    tipe = int(sig_tuple[1])
                     # The test can be wrong if there are *several*
                     # signatures (isc.org does that) and others are
                     # done with an older key. A validating resolver
@@ -73,7 +77,18 @@ def test_zone(zone):
                     # unvalidatable sigs because of a missing key, see
                     # RFC 4035 4.3 and 5. It is legal to have broken
                     # chains of trust.
-                    print "ERROR: signature of zone %s first seen at %s while the last keyset before key %s was last seen at %s and its TTL was %i" % (zone, first_sig, key_tag, reference_time, ttl)
+                    if verbose:
+                        print "\t\tSearching sig done on %s for type %s like %s..." % (first_sig, tipe, key_tag)
+                    resig_cursor.execute("""SELECT key_tag FROM Signatures
+                                 WHERE name=? AND type=? AND key_tag!=? AND first_seen=?;""",
+                                         [zone, tipe, key_tag, first_sig])
+                    doublesig_tuple = resig_cursor.fetchone()
+                    if doublesig_tuple is None:
+                        if order == 'name':
+                            index = zone + reference_time
+                        else:
+                            index = reference_time + zone
+                        problems[index] = "ERROR: signature of zone %s first seen at %s while the last keyset before key %s was last seen at %s and its TTL was %i" % (zone, first_sig, key_tag, reference_time, ttl)
             if verbose:
                 print "\tFor key %s last seen at %s..." % (key_tag, last_seen)
             keyset_cursor.execute("""SELECT DISTINCT Keysets.id, first_seen, last_seen
@@ -100,7 +115,7 @@ def test_zone(zone):
             if reference_time is not None:
                 # Otherwise, it simply means the key was not retired yet
                 # 48 : DNSKEY, a special case
-                sig_cursor.execute("""SELECT last_seen, ttl FROM Signatures
+                sig_cursor.execute("""SELECT last_seen, type, ttl FROM Signatures
                                           WHERE key_tag=? AND name=?
                                              AND type != 48 
                                              AND datetime(strftime('%s',last_seen)+ttl,
@@ -111,11 +126,26 @@ def test_zone(zone):
                                    [key_tag, zone, reference_time])
                 for sig_tuple in sig_cursor.fetchall():
                     last = sig_tuple[0]
-                    ttl = int(sig_tuple[1])
-                    print "ERROR: signature of zone %s last seen at %s (with a TTL of %i) while the key %s was retired at %s" % (zone, last, ttl, key_tag, reference_time)
+                    tipe = int(sig_tuple[1])
+                    ttl = int(sig_tuple[2])
+                    if verbose:
+                        print "\t\tSearching sig done on %s for type %s like %s..." % (last, tipe, key_tag)
+                    # Test for the case of a double-signature
+                    resig_cursor.execute("""SELECT key_tag FROM Signatures
+                                 WHERE name=? AND type=? AND key_tag!=? AND last_seen=?;""",
+                                         [zone, tipe, key_tag, last])
+                    doublesig_tuple = resig_cursor.fetchone()
+                    if doublesig_tuple is None:
+                        if order == 'name':
+                            index = zone + reference_time
+                        else:
+                            index = reference_time + zone
+                        problems[index] = "ERROR: signature of zone %s last seen at %s (with a TTL of %i) while the key %s was retired at %s" % (zone, last, ttl, key_tag, reference_time)
 
+if order != 'name' and order != 'time':
+    print "Invalid sorting criteria \"%s\"" % order
 if len(sys.argv) <= 1:
-    zone_cursor.execute("SELECT DISTINCT name from Zones ORDER BY name;")
+    zone_cursor.execute("SELECT DISTINCT name from Zones;")
     for zone_tuple in zone_cursor.fetchall():
         zone = zone_tuple[0]
         test_zone(zone)
@@ -124,6 +154,10 @@ else:
         if zone[-1:len(zone)] != '.':
             zone = zone + '.'
         test_zone(zone)
+problem_keys = problems.keys()
+problem_keys.sort()
+for problem in problem_keys:
+    print problems[problem]
 database.close()
 
 
